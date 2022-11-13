0509cfd7d43a89a041b3e418bb23b3bb96c67fda
ndinihasanah <ndinihasanah12@gmail.com>
2022-11-13 10:43:20 +0800

    Final Commit

---
+++ app/src/main/java/com/example/lab2c_nurdinihasanah/FullImageActivity.java
@@ -0,0 +1,26 @@
+package com.example.lab2c_nurdinihasanah;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+public class FullImageActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstaceState){
+        super.onCreate(savedInstaceState);
+        setContentView(R.layout.full_image);
+
+        Intent i = getIntent();
+
+        int position = i.getExtras().getInt("id");
+        ImageAdapter imageAdapter = new ImageAdapter(this);
+
+        ImageView imageView = (ImageView) findViewById(R.id.full_image_view);
+        imageView.setImageResource(imageAdapter.thumbImages[position]);
+
+        Toast.makeText(this,"Full view image " + position, Toast.LENGTH_SHORT).show();
+    }
+}
